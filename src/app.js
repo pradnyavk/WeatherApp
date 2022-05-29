561e87191fc812a182f2d2715f8810a5d2f95132
pradnya <pradnya.katigar007@gmail.com>
2022-05-29 23:38:02 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./geocode')
 const forecast = require('./forecast')
 
+const port = process.env.PORT || 3000
+
 const app = express()
 const publicPath = path.join(__dirname,'../public')
 
@@ -84,6 +86,6 @@ app.get('*',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('Started on port 3000')
+app.listen(port,()=>{
+    console.log('Started on port '+port)
 })
